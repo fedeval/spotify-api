ff262ea351c515f71addb46e25312b4132c5f15c
fedeval <federico.valenza5@gmail.com>
2021-07-25 16:03:57 +0300

    test: flask run in debug mode with autoreload

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask
 from authorize import authorize
 import requests
 
-
 app = Flask(__name__)
+
 base_url = '	https://api.spotify.com'
 
 @app.route('/')
 def hello_world():
-  return '<p>Hello, World!</p>'
+  return '<p>Hello, Federico!</p>'
 
 @app.route('/get-album/<id>')
 def get_album(id):
